77cd2dfc94924fcab7e7d1223937a4d177844655
Wiktor Gała <wiktor.gala@grupawp.pl>
2021-01-24 13:36:15 +0100

    remove comment

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from helpers.get_frequency_of_chars import get_frequency_of_chars
 from classes import Leaf, Huffman
 
 all_lines = get_text_from_file("./example-text-file.txt")
-chars_frequency = get_frequency_of_chars(all_lines) # reverse array
+chars_frequency = get_frequency_of_chars(all_lines)
 leaf_arr = []
 
 for el in chars_frequency:
